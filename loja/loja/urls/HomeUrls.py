7d86a605f0b37759be1dbf55c73cf190af7776b8
sarahmds <sarah.santos@escolar.ifrn.edu.br>
2025-05-27 14:33:43 -0300

    Add files via upload

--- loja/loja/urls/HomeUrls.py
+++ loja/loja/urls/HomeUrls.py
@@ -1,5 +1,5 @@
-from django.urls import path
+from django.urls import path #type: ignore
 from loja.views.HomeView import home_view
 urlpatterns = [
-    path("", home_view),
+    path("", home_view, name= 'home'),
 ]
--- loja/loja/views/HomeView.py
+++ loja/loja/views/HomeView.py
@@ -1,3 +1,8 @@
-from django.http import HttpResponse
+from django.shortcuts import render #type: ignore
+from loja.models import Produto
+produto = request.GET.get("produto") #type: ignore
+produtos = Produto.objects.all()
+if produto is not None:
+    produtos = produtos.filter(Produto__contains=produto)
 def home_view(request):
-    return HttpResponse('<h1>Olá mundo!</h1>')
+    return render(request, template_name='home/home.html', status=200)
